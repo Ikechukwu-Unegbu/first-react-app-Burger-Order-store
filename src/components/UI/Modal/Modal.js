d8fcc8533b49951120c08d7407b570fe55b2ec61
Ikechukwu-Unegbu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2021-01-31 02:16:09 +0100

    First ReactJs App

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './Modal.css';
+import Auxi from '../../../hoc/Auxi';
+import Backdrop from '../Backdrop/Backdrop';
+
+
+const modal = (props) => (
+  <Auxi> 
+    <Backdrop show={props.show}
+            clicked={props.modalClosed}/>
+    <div 
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1':'0'
+          }}
+          className={classes.Modal}>
+
+      {props.children}
+    </div>
+  </Auxi>
+ 
+);
+
+export default modal;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,121 @@
+import React, {Component} from 'react';
+import Auxi from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+const INGREDIENT_PRICES ={
+  salad:0.5,
+  cheese:1,
+  meat:2,
+  bacon:0.7
+};
+
+class BurgerBuilder extends Component{
+  // constructor(props){
+  //   super(props);
+  //   this.state ={...}
+  // }
+
+  state = {
+    ingredients:{
+      salad:0,
+      bacon:0,
+      cheese:0,
+      meat:0
+    },
+    totalPrice:4,
+    purchasable:false,
+    purchasing:false
+  }
+
+  uptdatePurchaseState(ingredients){
+   
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el)=>{
+        return sum + el;
+      }, 0);
+      this.setState({purchasable:sum>0});
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceDeduction;
+
+    this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
+    this.uptdatePurchaseState(updatedIngredients);
+  }
+
+  removeIngredientHandler = (type) =>{
+    const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0){
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
+    this.uptdatePurchaseState(updatedIngredients);
+  }
+  
+  purchaseHandler =() =>{
+    this.setState({purchasing:true});
+  }
+
+  purchaseCancelHandler = () =>{
+    this.setState({purchasing:false});
+  }
+
+  purchaseContinueHandler = () =>{
+    alert('you continue!');
+  }
+
+  render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for(let key in disabledInfo){
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+    return (
+      <Auxi>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary 
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinue={this.purchaseContinueHandler}
+            price={this.state.totalPrice}/>
+        </Modal>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls 
+          ingredientsAdded={this.addIngredientHandler}
+          ingredientsRemoved={this.removeIngredientHandler} 
+          disabled={disabledInfo}
+          purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler}
+          price={this.state.totalPrice}/>
+      </Auxi>
+    );
+  }
+}
+ 
+export default BurgerBuilder;
